e188af9f939a19118b73a2c40c790f983e09f4c8
obdzialoj <78039091+obdzialoj@users.noreply.github.com>
2023-04-04 21:27:17 -0400

    Update auth.provider.tsx

--- providers/auth/auth.provider.tsx
+++ providers/auth/auth.provider.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 import { User } from '../../models/user.model';
 import { AuthContext } from './auth.context';
 
@@ -7,8 +8,10 @@ export interface AuthContextProviderProps {
 }
 
 export const AuthContextProvider: FC<AuthContextProviderProps> = ({ children }): JSX.Element => {
+  const { checkExistingAuthorization } = useAuth();
+  const existingToken = checkExistingAuthorization();
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const [authToken, setAuthToken] = useState<string>(null);
+  const [authToken, setAuthToken] = useState<string>(existingToken);
 
   return (
     <AuthContext.Provider value={{ currentUser, setCurrentUser, setAuthToken, authToken }}>
